b59c434b1ebd27353294eac09aadb4dec4a4ea75
zhanglu1227 <dmaozl@sina.com>
2019-06-05 23:13:03 +0800

    复制excel对象--更改了部分代码

--- common/write_excel.py
+++ common/write_excel.py
@@ -14,26 +14,34 @@ class WriteExcel(object):
 
     dir = 'testData'
     excel_dir = os.path.dirname(os.getcwd()) + '/' + dir
-    excel_dir = os.getcwd() + '/' + dir
-    print('excel_dir',excel_dir)
+    # excel_dir = os.getcwd() + '/' + dir
+    print('excel_dir', excel_dir)
+    # 第一步：读取原excel中的所有数据（复制对象）
     rb = xlrd.open_workbook(excel_dir + '/' + 'data.xls')
+    # 第二步：复制读取的原excel对象
     wb = copy(rb)
-    # 通过get_sheet()获取的sheet有write()方法
+    # 第三步：通过get_sheet()获取复制对象的sheet页
     ws = wb.get_sheet(2)
 
-    def writeData(self,id,real,status):
+    # def __str__(self):
+    #     return '我在哪%s'%self.excel_dir
+
+    def writeData(self, id, real, status):
         try:
             print('写入')
             # 根据id写入对应的实际结果和接口测试状态
+            # 第四步：对sheet页进行写入(传入x和y坐标，和具体写入的value)
             self.ws.write(id, 2, real)
             self.ws.write(id, 3, status)
+
+            # 第五步：保存excel（具体的excel路径+名称）
             self.wb.save(self.excel_dir + '/' + 'data.xls')
             return 'ok'
 
         except Exception as msg:
             print(msg)
 
-# print(os.getcwd())
-# print(os.path.abspath(sys.argv[0]))
-# print(os.path.dirname(os.path.realpath(__file__)))
 
+if __name__ == '__main__':
+    a = WriteExcel()
+    print(a.writeData(0, 0, 'Success'))
